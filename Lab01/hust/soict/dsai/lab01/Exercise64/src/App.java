8e3813c743bf8ae1ead956f0940073534e10977c
ProgrammingForFun <phanmemmaytinhtql@gmail.com>
2021-04-08 03:48:06 +0700

    Lab03 Ex9

--- Lab01/hust/soict/dsai/lab01/Exercise64/src/App.java
+++
@@ -1,7 +0,0 @@
-package Exercise64.src;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
--- Lab01/hust/soict/dsai/lab01/Exercise65/src/App.java
+++
@@ -1,7 +0,0 @@
-package Exercise65.src;
-
-public class App {
-    public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-    }
-}
